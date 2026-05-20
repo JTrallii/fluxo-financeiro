e19db063982cb12252d00420512364c3a7547335
Jason <jtrallii@live.com>
2026-05-20 17:27:30 -0300

    feat: adicionando os arquivos do backend

---
+++ backend/src/main/java/Faculdade/projeto/Fluxo/Financeiro/FluxoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package Faculdade.projeto.Fluxo.Financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Faculdade/projeto/Fluxo/Financeiro/FluxoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package Faculdade.projeto.Fluxo.Financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
